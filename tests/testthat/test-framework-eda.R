ace39d9e1afada13a73a72fcdefa38c6bdaacaf3
Riley Wheadon <rileywheadon@gmail.com>
2025-08-15 11:01:24 -0600

    Small bugfix to make tests pass.

--- tests/testthat/test-framework-eda.R
+++ tests/testthat/test-framework-eda.R
@@ -6,7 +6,7 @@ test_that("framework-eda.R works on KOOTENAI RIVER (08NH021) with no splits", {
 	expect_true(is.list(results))
 	expect_equal(length(results$submodules), 2)
 
-	recommendations <- results$recommendations
+	recommendations <- results$eda_recommendations
 	expect_equal(recommendations$approach, "Piecewise NS-FFA")
 	expect_equal(recommendations$ns_splits, 1972)
 	expect_equal(recommendations$ns_structures, list(S01))
@@ -21,7 +21,7 @@ test_that("framework-eda.R works on KOOTENAI RIVER (08NH021) with splits", {
 	expect_true(is.list(results))
 	expect_equal(length(results$submodules), 3)
 
-	recommendations <- results$recommendations
+	recommendations <- results$eda_recommendations
 	expect_equal(recommendations$approach, "Piecewise NS-FFA")
 	expect_equal(recommendations$ns_splits, 1972)
 	expect_equal(recommendations$ns_structures, list(S10, S10))
